54576ee9726f5496964e0a6c429f65a4fb8c1ad1
Bopster411 <serg.dr2004@gmail.com>
2024-08-01 13:55:22 +0300

    Fixed deploy build

--- src/utils/api/config/index.constants.ts
+++ src/utils/api/config/index.constants.ts
@@ -1,4 +1,10 @@
+require('dotenv').config();
+
+const MODE = process.env.DEPLOY_MODE;
+
 const BACKEND_DOMAIN = 'productsapp-54xs.onrender.com/api';
 
-export const BACKEND_URL = 'http://localhost:3030/api';
-// export const BACKEND_URL = 'https://' + BACKEND_DOMAIN;
+export const BACKEND_URL =
+    MODE === 'development'
+        ? 'http://localhost:3030/api'
+        : 'https://' + BACKEND_DOMAIN;
